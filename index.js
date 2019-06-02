c5cc974c3a364779d3487f8457a8557aeec276c2
Majed Amin <majedamin974@gmail.com>
2019-06-02 11:18:29 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {};
+// function updateObjectWithKeyAndValue(object, key, value) {
+//   return Object.assign({}, object, {key: value})
+// }
+
+ function updateObjectWithKeyAndValue(object, key, value) {
+   object[key] = value;
+   return object;
+ }
+
+ function updateObjectWithKeyAndValue(object, key, value) {
+   var newObj = Object.assign({}, object, {[key]: value});
+   return newObj;
+ }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
+}
+ // function functionName() {
+ //
+ // }
